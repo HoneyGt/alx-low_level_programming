50d7b88f6b6daf9dffc91af0b5f3571c98ec6c06
Honey Gt <nadhigutu17@gmail.com>
2023-02-06 10:17:20 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int o;
 	int nletters;
-	int rwr;
+	int w;
 
 	if (!filename)
 		return (-1);
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-	rwr = write(o, text_content, nletters);
-	if (rwr == -1)
+	w = write(o, text_content, nletters);
+	if (w == -1)
 		return (-1);
 	close(o);
 	return (1);
