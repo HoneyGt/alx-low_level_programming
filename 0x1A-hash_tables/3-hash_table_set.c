8ef32240b6f2aecd6792e3277c831a5d99484039
HoneyGt <nadhigutu@gmail.com>
2023-06-09 09:48:03 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_set - Adds an element to a hash table
  * @ht: A pointer to the hash table
- * @key: The key to add - can't be an empty string
+ * @key: The key to add - cannot be an empty string
  * @value: The value associated with key
  *
  * Return: Upon failure - 0
